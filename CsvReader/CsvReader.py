b189220d585af68e5a4cf0f851e5aab99a7ef87c
mykolatyniv <ml546@njit.edu>
2019-11-21 11:20:50 -0500

    Task: Added new files

--- CsvReader/CsvReader.py
+++ CsvReader/CsvReader.py
@@ -1,20 +1,21 @@
 import csv
 from Fileutilities.absolutepath import absolutepath
-from pprint import pprint
+
 
 def ClassFactory(class_name, dictionary):
     return type(class_name, (object,), dictionary)
+
 
 class CsvReader:
     data = []
 
     def __init__(self, filepath):
         self.data = []
+
         with open(absolutepath(filepath)) as text_data:
             csv_data = csv.DictReader(text_data, delimiter=',')
             for row in csv_data:
                 self.data.append(row)
-                pprint(self.data)
         pass
 
     def return_data_as_objects(self, class_name):
