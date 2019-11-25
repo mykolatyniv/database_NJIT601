46c155bd5acd7bc3b92268c21bc346afba41fe11
mykolatyniv <ml546@njit.edu>
2019-11-25 17:23:14 -0500

    Task: added homework steps and files

---
+++ Database/homework2.py
@@ -0,0 +1,34 @@
+from sqlalchemy import update
+from sqlalchemy.exc import IntegrityError
+from datetime import datetime
+ 
+ 
+def dispatch_order(order_id):
+ 
+    # check whether order_id is valid or not
+    order = session.query(Order).get(order_id)
+    
+    if not order:
+        raise ValueError("Invalid order id: {}.".format(order_id))    
+        
+    if order.date_shipped:
+        print("Order already shipped.")
+        return
+ 
+    try:
+        for i in order.order_lines:
+            i.item.quantity = i.item.quantity - i.quantity            
+        
+        order.date_shipped = datetime.now()                            
+        session.commit()
+        print("Transaction completed.")
+ 
+    except IntegrityError as e:
+        print(e)
+        print("Rolling back ...")
+        session.rollback()
+        print("Transaction failed.")
+
+dispatch_order(1)
+
+dispatch_order(2)
