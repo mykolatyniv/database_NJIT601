f0659c1765671ab33876d468d335d211cb1ca878
mykolatyniv <ml546@njit.edu>
2019-11-26 11:21:30 -0500

    Task: Added info

--- Database/sqlalchemy_test.py
+++ Database/sqlalchemy_test.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Numeric
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
